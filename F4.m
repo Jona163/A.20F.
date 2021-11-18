8c45c8de73b218275fa656b4d88eabab294c8c66
Jona163 <91714580+Jona163@users.noreply.github.com>
2021-11-17 19:49:01 -0600

    Add files via upload

---
+++ F4.m
@@ -0,0 +1,21 @@
+% octave.scriptFuncional.
+% Escuela: Tecnologico de Estudios Superiores de Jilotepec.
+% Especialidad: Ingeniería en Sistemas Computacionales.
+% Titulo:Funciones Algebraicas:Polinomiales y Racionales.
+% Descripción:Demostar Funciones. 
+% Autor:Jonathan Zuar Hernandez Mayen. 
+% Fecha:17-11-2021
+% Versión: 1.0.1
+% Notas:La actividad cuenta de 6 ejercicios, esta es la 4 de 6.
+% Funcion 4/6
+
+clear
+
+%syms x
+x=[-10:1:10]
+%Funcion a Plotear
+fx=(2.*x + 1)
+plot(x,fx)
+%Tipo de Funcion.
+grid on;
+title("Funcion Polinomial f(X)=(-0.5,0)");
